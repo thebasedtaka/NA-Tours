24b98310bf17f9298ab2400d927e891e9e3db580
thebasedtaka <darktaka9998@gmail.com>
2024-01-02 20:10:42 -0500

    deployment commit

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//import '@babel/polyfill';
+import '@babel/polyfill';
 // import 'core-js/stable';
 // import 'regenerator-runtime/runtime.js';
 import { showAlert } from './alerts';
